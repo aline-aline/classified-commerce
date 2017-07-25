90a064d8c1498919d7b20fa139a102bde974f0fa
aline-aline <alinecris@gmail.com>
2017-07-24 21:57:23 -0300

    Add categories

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  layout :layout_by_resource
+
+  protected
+
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "backoffice_devise"
+    else
+      "application"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  
+  get 'backoffice', to: 'backoffice/dashboard#index'
+  
+  namespace :backoffice do
+    resources :categories, except: [:show, :destroy]
+    get 'dashboard', to: 'dashboard#index'
+  end
+
   namespace :site do
     get 'home', to: 'home/index'
   end
 
-  get 'dashboard', to: 'backoffice/dashboard#index'
-
-  namespace :backoffice do
-    get 'dashboard', to: 'dashboard#index'
-  end
-
   devise_for :admins
   devise_for :members
+
   root 'site/home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
