76e2c33a7a6cb6995daef7c3f234ee2df06861e0
aline-aline <alinecris@gmail.com>
2017-08-03 22:21:42 -0300

    Configure emailing through modal window

--- app/controllers/backoffice/send_mail_controller.rb
+++ app/controllers/backoffice/send_mail_controller.rb
@@ -1,2 +1,18 @@
 class Backoffice::SendMailController < ApplicationController
+
+  def edit
+    @admin = Admin.find(params[:id])
+    
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def create
+    AdminMailer.send_message(current_admin, params[:'recipient-text'], params[:'subject-text'], params[:'message-text']).deliver_now
+
+    respond_to do |format|
+      format.js
+    end
+  end
 end
