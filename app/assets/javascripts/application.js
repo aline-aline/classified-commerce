359391b76eb2b2b08e34729c4f05ed91bfde6931
aline-aline <alinecris@gmail.com>
2017-08-22 21:42:18 -0300

    Load ads through ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,16 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap.growl
+
+
+/* Spin */
+$(document).ready(function() {
+    // Global ajax cursor change
+    $(document)
+        .ajaxStart(function() {
+            $('#global-spin').fadeIn('slow');
+        })
+        .ajaxStop(function() {
+            $('#global-spin').fadeOut('slow');
+        });
+});
