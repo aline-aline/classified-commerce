e3d67ea4f28c1e04d0c1b2d823d1c475bfcf9842
aline-aline <alinecris@gmail.com>
2018-04-24 21:43:51 -0300

    Redirect pages after login with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
-  
+  before_filter :store_current_location, :unless => :devise_controller?
+
   # Pundit
   include Pundit
 
@@ -13,18 +14,24 @@ class ApplicationController < ActionController::Base
 
   protected
 
-    def layout_by_resource
-      if devise_controller? && resource_name == :admin
-        "backoffice_devise"
-      elsif devise_controller? && resource_name == :member
-        "site_devise"
-      else
-        "application"
-      end
+  def layout_by_resource
+    if devise_controller? && resource_name == :admin
+      "backoffice_devise"
+    elsif devise_controller? && resource_name == :member
+      "site_devise"
+    else
+      "application"
     end
+  end
 
-    def user_not_authorized
-      flash[:alert] = "You are not authorized to perform this action."
-      redirect_to(request.referrer || root_path)
-    end
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
+  end
+
+  private
+
+  def store_current_location
+    store_location_for(:member, request.url) 
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
       resources :ads, only: [:index, :edit, :update, :new, :create]
     end
 
-    resources :ad_details, only: [:show] 
+    resources :ad_details, only: [:index, :show] 
     resources :categories, only: [:show]
     resources :comments, only: [:create]
   end
